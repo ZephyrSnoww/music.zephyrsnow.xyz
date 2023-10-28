d95cb3bc3f92ed40fd8e4e3fcc2e58bd5a75d8e8
Zephyr <zephyr.snow@pm.me>
2023-10-27 19:37:47 -0600

    Created app framework

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script setup>
+import { reactive } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
+
+let state = reactive({});
 </script>
 
 <template>
-  <RouterView />
+  <div class="left-bar">
+
+  </div>
+
+  <div class="bottom-bar">
+
+  </div>
+
+  <div class="main-view">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
+.left-bar {
+  grid-area: left;
+  z-index: 5;
+  box-shadow: 0px 0px 15px #0008;
+}
 
+.bottom-bar {
+  grid-area: bottom;
+  z-index: 10;
+  background-color: var(--background);
+  box-shadow: 0px 0px 15px #0008;
+}
+
+.main-view {
+  grid-area: main;
+}
 </style>
